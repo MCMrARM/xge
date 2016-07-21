ebb05f7f7c594765bef866182bec3da2ce0d60a9
MCMrARM <mcmrarm@gmail.com>
2016-07-21 21:23:42 +0200

    MeshBuilder::triangle() and MeshBuilder::quad()

--- engine/xge/render/MeshBuilder.cpp
+++ engine/xge/render/MeshBuilder.cpp
@@ -1,8 +1,10 @@
 #include "MeshBuilder.h"
+
 #include <stdarg.h>
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
+#include <xge/util/Log.h>
 
 using namespace xge;
 
@@ -10,6 +12,18 @@ using namespace xge;
         ShaderValue shaderValType; \
         shaderValType.f = val; \
         vector->push_back(shaderValType); \
+    }
+#define pushValTypeVec2(vector, val) { \
+        pushValTypeFloat(vector, val.x); \
+        pushValTypeFloat(vector, val.y); \
+    }
+#define pushValTypeVec3(vector, val) { \
+        pushValTypeVec2(vector, val); \
+        pushValTypeFloat(vector, val.z); \
+    }
+#define pushValTypeVec4(vector, val) { \
+        pushValTypeVec3(vector, val); \
+        pushValTypeFloat(vector, val.w); \
     }
 
 MeshBuilder &MeshBuilder::push(...) {
@@ -23,26 +37,20 @@ MeshBuilder &MeshBuilder::push(...) {
                 pushValTypeFloat(itV, va_arg(args, double));
                 break;
             case ShaderValueType::Vec2: {
-                glm::vec2 v2 = va_arg(args, glm::vec2);
-                pushValTypeFloat(itV, v2.x);
-                pushValTypeFloat(itV, v2.y);
-                break;
+                auto val = va_arg(args, glm::vec2);
+                pushValTypeVec2(itV, val);
             }
+                break;
             case ShaderValueType::Vec3: {
-                glm::vec3 v3 = va_arg(args, glm::vec3);
-                pushValTypeFloat(itV, v3.x);
-                pushValTypeFloat(itV, v3.y);
-                pushValTypeFloat(itV, v3.z);
-                break;
+                auto val = va_arg(args, glm::vec3);
+                pushValTypeVec3(itV, val);
             }
+                break;
             case ShaderValueType::Vec4: {
-                glm::vec4 v4 = va_arg(args, glm::vec4);
-                pushValTypeFloat(itV, v4.x);
-                pushValTypeFloat(itV, v4.y);
-                pushValTypeFloat(itV, v4.z);
-                pushValTypeFloat(itV, v4.w);
-                break;
+                auto val = va_arg(args, glm::vec4);
+                pushValTypeVec4(itV, val);
             }
+                break;
             default:
                 continue;
         }
@@ -51,16 +59,69 @@ MeshBuilder &MeshBuilder::push(...) {
     return *this;
 }
 
+void MeshBuilder::triangle(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3, glm::vec2 uv1, glm::vec2 uv2, glm::vec2 uv3,
+                           glm::vec4 color1, glm::vec4 color2, glm::vec4 color3) {
+    XGEAssert(mesh->drawMode == xge::Mesh::DrawMode::TRIANGLES);
+    auto itV = attributeValues.begin();
+    auto itU = attributeUsages.begin();
+    vertexCount += 3;
+    for (auto it = attributes.begin(); it != attributes.end() && itV != attributeValues.end() &&
+            itU != attributeUsages.end(); it++, itV++, itU++) {
+        AttributeUsage usage = *itU;
+        switch (usage) {
+            case AttributeUsage::POSITION:
+                if (it->type == ShaderValueType::Vec3) {
+                    pushValTypeVec3(itV, pos1);
+                    pushValTypeVec3(itV, pos2);
+                    pushValTypeVec3(itV, pos3);
+                } else if (it->type == ShaderValueType::Vec2) {
+                    pushValTypeVec2(itV, pos1);
+                    pushValTypeVec2(itV, pos2);
+                    pushValTypeVec2(itV, pos3);
+                } else {
+#ifndef NDEBUG
+                    Log::error("MeshBuilder", "Invalid position attribute type");
+#endif
+                    abort();
+                }
+                break;
+            case AttributeUsage::TEXTURE_UV:
+                pushValTypeVec2(itV, uv1);
+                pushValTypeVec2(itV, uv2);
+                pushValTypeVec2(itV, uv3);
+                break;
+            case AttributeUsage::COLOR:
+                pushValTypeVec4(itV, color1);
+                pushValTypeVec4(itV, color2);
+                pushValTypeVec4(itV, color3);
+                break;
+            default: {
+#ifndef NDEBUG
+                Log::error("MeshBuilder", "AttributeUsage::CUSTOM is not allowed when using MeshBuilder::triangle");
+#endif
+                abort();
+            }
+        }
+    }
+}
+
+void MeshBuilder::quad(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3, glm::vec3 pos4,
+                       glm::vec2 uv1, glm::vec2 uv2, glm::vec2 uv3, glm::vec2 uv4,
+                       glm::vec4 color1, glm::vec4 color2, glm::vec4 color3, glm::vec4 color4) {
+    triangle(pos3, pos2, pos1, uv3, uv2, uv1, color3, color2, color1);
+    triangle(pos3, pos1, pos4, uv3, uv1, uv4, color3, color1, color4);
+}
+
 std::shared_ptr<Mesh> MeshBuilder::build(bool reuse) {
     std::shared_ptr<Mesh> ret = std::move(mesh);
     ret->vertexCount = vertexCount;
     auto itV = attributeValues.begin();
-    auto itU = attributeUsages.begin();
+    auto itBU = attributeBufferUsages.begin();
     for (auto it = attributes.begin(); it != attributes.end() && itV != attributeValues.end() &&
-            itU != attributeUsages.end(); it++, itV++, itU++) {
+            itBU != attributeBufferUsages.end(); it++, itV++, itBU++) {
         Mesh::AttributeValue val;
         val.attribute = *it;
-        val.buffer.upload(it->type, *itV, *itU);
+        val.buffer.upload(it->type, *itV, *itBU);
         ret->attributes[it->attributeId] = std::move(val);
     }
     if (reuse) {
--- engine/xge/render/MeshBuilder.h
+++ engine/xge/render/MeshBuilder.h
@@ -10,10 +10,16 @@ namespace xge {
 
     class MeshBuilder {
 
+    public:
+        enum class AttributeUsage {
+            POSITION, COLOR, TEXTURE_UV, CUSTOM
+        };
+
     protected:
         std::shared_ptr<Mesh> mesh;
         std::vector<ShaderAttribute> attributes;
-        std::vector<GPUBuffer::Usage> attributeUsages;
+        std::vector<AttributeUsage> attributeUsages;
+        std::vector<GPUBuffer::Usage> attributeBufferUsages;
         std::vector<std::vector<ShaderValue>> attributeValues;
         unsigned int vertexCount = 0;
 
@@ -28,15 +34,24 @@ namespace xge {
             mesh->drawMode = drawMode;
         }
 
-        inline MeshBuilder &addAttribute(ShaderAttribute attribute, GPUBuffer::Usage usage = GPUBuffer::Usage::Static) {
+        /**
+         * Add a uniform to the builder. This method should not be called if any vertex was already pushed to the list.
+         * The attribute usage is how you use the attribute in shader. Please use the correct one and if you don't see
+         * your usage on the list use AttributeUsage::CUSTOM. If you don't specify the attribute usage or use
+         * AttributeUsage::CUSTOM, you'll not be able to call ::vertex() or ::quad().
+         */
+        inline MeshBuilder &addAttribute(ShaderAttribute attribute, AttributeUsage usage = AttributeUsage::CUSTOM,
+                                         GPUBuffer::Usage bufferUsage = GPUBuffer::Usage::Static) {
             attributes.push_back(attribute);
             attributeUsages.push_back(usage);
+            attributeBufferUsages.push_back(bufferUsage);
             attributeValues.push_back({});
             return *this;
         }
         inline MeshBuilder &addAttribute(std::string attributeName, ShaderValueType valType,
-                                         GPUBuffer::Usage usage = GPUBuffer::Usage::Static) {
-            return addAttribute(mesh->program->getAttribute(attributeName, valType), usage);
+                                         AttributeUsage usage = AttributeUsage::CUSTOM,
+                                         GPUBuffer::Usage bufferUsage = GPUBuffer::Usage::Static) {
+            return addAttribute(mesh->program->getAttribute(attributeName, valType), usage, bufferUsage);
         }
 
         /* wrappers for Mesh's setUniform */
@@ -114,6 +129,54 @@ namespace xge {
          * This function pushes a vertex onto the list. The argument count should equal the count of attributes.
          */
         MeshBuilder &push(...);
+
+        void triangle(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3, glm::vec2 uv1, glm::vec2 uv2, glm::vec2 uv3,
+                      glm::vec4 color1, glm::vec4 color2, glm::vec4 color3);
+        inline void triangle(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3) {
+            triangle(pos1, pos2, pos3, glm::vec2(0.f), glm::vec2(0.f), glm::vec2(0.f), glm::vec4(1.f), glm::vec4(1.f),
+                     glm::vec4(1.f));
+        }
+        inline void triangleUV(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3,
+                               glm::vec2 uv1, glm::vec2 uv2, glm::vec2 uv3) {
+            triangle(pos1, pos2, pos3, uv1, uv2, uv3, glm::vec4(1.f), glm::vec4(1.f), glm::vec4(1.f));
+        }
+        inline void triangleColor(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3,
+                                  glm::vec4 color1, glm::vec4 color2, glm::vec4 color3) {
+            triangle(pos1, pos2, pos3, glm::vec2(0.f), glm::vec2(0.f), glm::vec2(0.f), color1, color2, color3);
+        }
+
+        // order: bottom left, top left, top right, top bottom
+        void quad(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3, glm::vec3 pos4,
+                  glm::vec2 uv1, glm::vec2 uv2, glm::vec2 uv3, glm::vec2 uv4,
+                  glm::vec4 color1, glm::vec4 color2, glm::vec4 color3, glm::vec4 color4);
+        inline void quad(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3, glm::vec3 pos4) {
+            quad(pos1, pos2, pos3, pos4, glm::vec2(0.f), glm::vec2(0.f), glm::vec2(0.f), glm::vec2(0.f),
+                 glm::vec4(1.f), glm::vec4(1.f), glm::vec4(1.f), glm::vec4(1.f));
+        }
+        inline void quadUV(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3, glm::vec3 pos4,
+                           glm::vec2 uv1, glm::vec2 uv2, glm::vec2 uv3, glm::vec2 uv4) {
+            quad(pos1, pos2, pos3, pos4, uv1, uv2, uv3, uv4, glm::vec4(1.f), glm::vec4(1.f), glm::vec4(1.f), glm::vec4(1.f));
+        }
+        inline void quadColor(glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3, glm::vec3 pos4,
+                              glm::vec4 color1, glm::vec4 color2, glm::vec4 color3, glm::vec4 color4) {
+            quad(pos1, pos2, pos3, pos4, glm::vec2(0.f), glm::vec2(0.f), glm::vec2(0.f), glm::vec2(0.f),
+                 color1, color2, color3, color4);
+        }
+
+        inline void rect(glm::vec2 pos1, glm::vec2 pos2, glm::vec2 uv1, glm::vec2 uv2,
+                           glm::vec4 color1, glm::vec4 color2, glm::vec4 color3, glm::vec4 color4) {
+            quad(glm::vec3(pos1.x, pos1.y, 0.f), glm::vec3(pos1.x, pos2.y, 0.f), glm::vec3(pos2.x, pos2.y, 0.f),
+                 glm::vec3(pos2.x, pos1.y, 0.f), uv1, glm::vec2(uv1.x, uv2.y), uv2, glm::vec2(uv2.x, uv1.y),
+                 color1, color2, color3, color4);
+        }
+        inline void rectUV(glm::vec2 pos1, glm::vec2 pos2, glm::vec2 uv1, glm::vec2 uv2) {
+            rect(pos1, pos2, uv1, uv2, glm::vec4(1.f), glm::vec4(1.f), glm::vec4(1.f), glm::vec4(1.f));
+        }
+        inline void rectColor(glm::vec2 pos1, glm::vec2 pos2,
+                           glm::vec4 color1, glm::vec4 color2, glm::vec4 color3, glm::vec4 color4) {
+            rect(pos1, pos2, glm::vec2(0.f, 0.f), glm::vec2(0.f, 0.f), color1, color2, color3, color4);
+        }
+
 
         /**
          * This function builds the mesh. If reuse is set to false, further use of this instance will be impossible.
